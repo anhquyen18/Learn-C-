ad0cbd5f285d06d7dc520a1bff2a0b2b9c3d95eb
anhquyen18 <anhquyen18092000@gmail.com>
2022-01-06 21:55:08 +0700

    Dialogs

---
+++ Bai52/Bai52/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace Bai52
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.chonHinhBt = new System.Windows.Forms.Button();
+            this.openFilePB = new System.Windows.Forms.PictureBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileRTB = new System.Windows.Forms.RichTextBox();
+            this.luuBt = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.chonMauBt = new System.Windows.Forms.Button();
+            this.tenLb = new System.Windows.Forms.Label();
+            this.doiFontBt = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pathTB = new System.Windows.Forms.TextBox();
+            this.chonFolderBt = new System.Windows.Forms.Button();
+            this.folderLuuBt = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.openFilePB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.openFilePB);
+            this.tabPage1.Controls.Add(this.chonHinhBt);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 421);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Open File Dialog";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.luuBt);
+            this.tabPage2.Controls.Add(this.saveFileRTB);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 421);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Save File Dialog";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.chonMauBt);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(792, 421);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Color Dialog";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.doiFontBt);
+            this.tabPage4.Controls.Add(this.tenLb);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(792, 421);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Font Dialog";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.folderLuuBt);
+            this.tabPage5.Controls.Add(this.chonFolderBt);
+            this.tabPage5.Controls.Add(this.pathTB);
+            this.tabPage5.Controls.Add(this.richTextBox1);
+            this.tabPage5.Controls.Add(this.label3);
+            this.tabPage5.Controls.Add(this.label2);
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(792, 421);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Folder Browser Dialog";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // chonHinhBt
+            // 
+            this.chonHinhBt.AutoSize = true;
+            this.chonHinhBt.Location = new System.Drawing.Point(58, 35);
+            this.chonHinhBt.Name = "chonHinhBt";
+            this.chonHinhBt.Size = new System.Drawing.Size(94, 27);
+            this.chonHinhBt.TabIndex = 0;
+            this.chonHinhBt.Text = "Chọn 1 hình";
+            this.chonHinhBt.UseVisualStyleBackColor = true;
+            this.chonHinhBt.Click += new System.EventHandler(this.chonHinhBt_Click);
+            // 
+            // openFilePB
+            // 
+            this.openFilePB.Location = new System.Drawing.Point(225, 56);
+            this.openFilePB.Name = "openFilePB";
+            this.openFilePB.Size = new System.Drawing.Size(321, 327);
+            this.openFilePB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.openFilePB.TabIndex = 1;
+            this.openFilePB.TabStop = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.Filter = "Tập tin png|*.png|Tập tin Bitmap|*.bmp|Tất cả|*.*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập nội dung:";
+            // 
+            // saveFileRTB
+            // 
+            this.saveFileRTB.Location = new System.Drawing.Point(198, 82);
+            this.saveFileRTB.Name = "saveFileRTB";
+            this.saveFileRTB.Size = new System.Drawing.Size(406, 220);
+            this.saveFileRTB.TabIndex = 1;
+            this.saveFileRTB.Text = "";
+            // 
+            // luuBt
+            // 
+            this.luuBt.AutoSize = true;
+            this.luuBt.Location = new System.Drawing.Point(347, 342);
+            this.luuBt.Name = "luuBt";
+            this.luuBt.Size = new System.Drawing.Size(75, 27);
+            this.luuBt.TabIndex = 2;
+            this.luuBt.Text = "Lưu";
+            this.luuBt.UseVisualStyleBackColor = true;
+            this.luuBt.Click += new System.EventHandler(this.luuBt_Click);
+            // 
+            // chonMauBt
+            // 
+            this.chonMauBt.AutoSize = true;
+            this.chonMauBt.Location = new System.Drawing.Point(336, 190);
+            this.chonMauBt.Name = "chonMauBt";
+            this.chonMauBt.Size = new System.Drawing.Size(82, 27);
+            this.chonMauBt.TabIndex = 0;
+            this.chonMauBt.Text = "Chọn màu";
+            this.chonMauBt.UseVisualStyleBackColor = true;
+            this.chonMauBt.Click += new System.EventHandler(this.chonMauBt_Click);
+            // 
+            // tenLb
+            // 
+            this.tenLb.AutoSize = true;
+            this.tenLb.Location = new System.Drawing.Point(260, 159);
+            this.tenLb.Name = "tenLb";
+            this.tenLb.Size = new System.Drawing.Size(113, 17);
+            this.tenLb.TabIndex = 0;
+            this.tenLb.Text = "Trần Anh Quyền";
+            // 
+            // doiFontBt
+            // 
+            this.doiFontBt.AutoSize = true;
+            this.doiFontBt.Location = new System.Drawing.Point(110, 22);
+            this.doiFontBt.Name = "doiFontBt";
+            this.doiFontBt.Size = new System.Drawing.Size(75, 27);
+            this.doiFontBt.TabIndex = 1;
+            this.doiFontBt.Text = "Đổi Font";
+            this.doiFontBt.UseVisualStyleBackColor = true;
+            this.doiFontBt.Click += new System.EventHandler(this.doiFontBt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nhập nội dung:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(53, 74);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(656, 176);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Chọn nơi lưu thông tin";
+            // 
+            // pathTB
+            // 
+            this.pathTB.Location = new System.Drawing.Point(86, 296);
+            this.pathTB.Name = "pathTB";
+            this.pathTB.Size = new System.Drawing.Size(475, 22);
+            this.pathTB.TabIndex = 2;
+            // 
+            // chonFolderBt
+            // 
+            this.chonFolderBt.Location = new System.Drawing.Point(636, 296);
+            this.chonFolderBt.Name = "chonFolderBt";
+            this.chonFolderBt.Size = new System.Drawing.Size(75, 23);
+            this.chonFolderBt.TabIndex = 3;
+            this.chonFolderBt.Text = "...";
+            this.chonFolderBt.UseVisualStyleBackColor = true;
+            this.chonFolderBt.Click += new System.EventHandler(this.chonFolderBt_Click);
+            // 
+            // folderLuuBt
+            // 
+            this.folderLuuBt.Location = new System.Drawing.Point(52, 356);
+            this.folderLuuBt.Name = "folderLuuBt";
+            this.folderLuuBt.Size = new System.Drawing.Size(75, 23);
+            this.folderLuuBt.TabIndex = 3;
+            this.folderLuuBt.Text = "Lưu thông tin";
+            this.folderLuuBt.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.openFilePB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox openFilePB;
+        private System.Windows.Forms.Button chonHinhBt;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button luuBt;
+        private System.Windows.Forms.RichTextBox saveFileRTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button chonMauBt;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button doiFontBt;
+        private System.Windows.Forms.Label tenLb;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button folderLuuBt;
+        private System.Windows.Forms.Button chonFolderBt;
+        private System.Windows.Forms.TextBox pathTB;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Bai52/Bai52/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai52
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chonHinhBt_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                openFilePB.Image = Image.FromFile(openFileDialog.FileName);
+            }
+        }
+
+        private void luuBt_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Plain Text|*.txt|Tất cả|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Bạn muốn lưu nội dung vào: " + saveFileDialog1.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Bạn không lưu");
+                
+            }
+        }
+
+        private void chonMauBt_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = tabControl1.SelectedTab.BackColor;
+            colorDialog1.AllowFullOpen = true;
+            colorDialog1.FullOpen = true;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tabControl1.SelectedTab.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void doiFontBt_Click(object sender, EventArgs e)
+        {
+            fontDialog1.Font = tenLb.Font;
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tenLb.Font = fontDialog1.Font;
+            }
+        }
+
+        private void chonFolderBt_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathTB.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
