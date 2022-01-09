ab4efcb21a5ce79a965d8d60160a5b30707e57c5
anhquyen18 <anhquyen18092000@gmail.com>
2022-01-09 21:35:56 +0700

    project Quan ly Sinh Vien

---
+++ Bai53/Bai53/Model/SinhVien.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai53
+{
+    class SinhVien
+    {
+        public string Ma { get; set; }
+        public string Ten { get; set; }
+        public DateTime NamSinh { get; set; }
+        public bool GioiTinh { get; set; }
+        public LopHoc LopChuQuan { get; set; }
+        public string GiaoVienChuNhiem { get; set; }
+    }
+}
