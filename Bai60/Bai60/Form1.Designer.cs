eecec264a6549b3c32a328e6ab30c1fc6e88be77
anhquyen18 <anhquyen18092000@gmail.com>
2022-01-23 10:01:35 +0700

    connect to sql

---
+++ Bai60/Bai60/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Bai60
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btKetNoi = new System.Windows.Forms.Button();
+            this.btDongKetNoi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btKetNoi
+            // 
+            this.btKetNoi.AutoSize = true;
+            this.btKetNoi.Location = new System.Drawing.Point(222, 52);
+            this.btKetNoi.Name = "btKetNoi";
+            this.btKetNoi.Size = new System.Drawing.Size(255, 68);
+            this.btKetNoi.TabIndex = 0;
+            this.btKetNoi.Text = "Kết nối CSDL";
+            this.btKetNoi.UseVisualStyleBackColor = true;
+            this.btKetNoi.Click += new System.EventHandler(this.btKetNoi_Click);
+            // 
+            // btDongKetNoi
+            // 
+            this.btDongKetNoi.AutoSize = true;
+            this.btDongKetNoi.Location = new System.Drawing.Point(222, 138);
+            this.btDongKetNoi.Name = "btDongKetNoi";
+            this.btDongKetNoi.Size = new System.Drawing.Size(255, 68);
+            this.btDongKetNoi.TabIndex = 0;
+            this.btDongKetNoi.Text = "Đóng kết nối";
+            this.btDongKetNoi.UseVisualStyleBackColor = true;
+            this.btDongKetNoi.Click += new System.EventHandler(this.btDongKetNoi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 336);
+            this.Controls.Add(this.btDongKetNoi);
+            this.Controls.Add(this.btKetNoi);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btKetNoi;
+        private System.Windows.Forms.Button btDongKetNoi;
+    }
+}
+
---
+++ Bai60/Bai60/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Bai60
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = null;
+        string strConn = "SERVER=PAIDEPTRAI\\SQLEXPRESS; Database = CSDL_QLDB; Integrated Security = True";
+        private void btKetNoi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(strConn);
+                conn.Open();
+                MessageBox.Show("Kết nối CSDL thành công");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btDongKetNoi_Click(object sender, EventArgs e)
+        {
+            if (conn != null && conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+                MessageBox.Show("Đóng kết nối CSDL thành công");
+            }
+        }
+    }
+}
