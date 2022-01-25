a36b9234533f6f150e12dab4c54bba0e57bff597
anhquyen18 <anhquyen18092000@gmail.com>
2022-01-25 15:11:12 +0700

    update sql

---
+++ Bai63/Bai63/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace Bai63
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lvSanPham = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbMa = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTen = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbGia = new System.Windows.Forms.TextBox();
+            this.btCapNhat = new System.Windows.Forms.Button();
+            this.cbbDanhMuc = new System.Windows.Forms.ComboBox();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btCapNhat2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh mục:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Danh sách sản phẩm:";
+            // 
+            // lvSanPham
+            // 
+            this.lvSanPham.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvSanPham.FullRowSelect = true;
+            this.lvSanPham.GridLines = true;
+            this.lvSanPham.HideSelection = false;
+            this.lvSanPham.Location = new System.Drawing.Point(129, 70);
+            this.lvSanPham.Name = "lvSanPham";
+            this.lvSanPham.Size = new System.Drawing.Size(270, 205);
+            this.lvSanPham.TabIndex = 2;
+            this.lvSanPham.UseCompatibleStateImageBehavior = false;
+            this.lvSanPham.View = System.Windows.Forms.View.Details;
+            this.lvSanPham.SelectedIndexChanged += new System.EventHandler(this.lvSanPham_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Mã:";
+            // 
+            // tbMa
+            // 
+            this.tbMa.Location = new System.Drawing.Point(129, 293);
+            this.tbMa.Name = "tbMa";
+            this.tbMa.Size = new System.Drawing.Size(270, 22);
+            this.tbMa.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Tên:";
+            // 
+            // tbTen
+            // 
+            this.tbTen.Location = new System.Drawing.Point(129, 330);
+            this.tbTen.Name = "tbTen";
+            this.tbTen.Size = new System.Drawing.Size(270, 22);
+            this.tbTen.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Giá:";
+            // 
+            // tbGia
+            // 
+            this.tbGia.Location = new System.Drawing.Point(129, 366);
+            this.tbGia.Name = "tbGia";
+            this.tbGia.Size = new System.Drawing.Size(270, 22);
+            this.tbGia.TabIndex = 1;
+            // 
+            // btCapNhat
+            // 
+            this.btCapNhat.AutoSize = true;
+            this.btCapNhat.Location = new System.Drawing.Point(129, 408);
+            this.btCapNhat.Name = "btCapNhat";
+            this.btCapNhat.Size = new System.Drawing.Size(141, 27);
+            this.btCapNhat.TabIndex = 3;
+            this.btCapNhat.Text = "Cập nhật sản phẩm";
+            this.btCapNhat.UseVisualStyleBackColor = true;
+            this.btCapNhat.Click += new System.EventHandler(this.btCapNhat_Click);
+            // 
+            // cbbDanhMuc
+            // 
+            this.cbbDanhMuc.FormattingEnabled = true;
+            this.cbbDanhMuc.Location = new System.Drawing.Point(129, 23);
+            this.cbbDanhMuc.Name = "cbbDanhMuc";
+            this.cbbDanhMuc.Size = new System.Drawing.Size(256, 24);
+            this.cbbDanhMuc.TabIndex = 4;
+            this.cbbDanhMuc.SelectedIndexChanged += new System.EventHandler(this.cbbDanhMuc_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên sản phẩm";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Đơn giá";
+            // 
+            // btCapNhat2
+            // 
+            this.btCapNhat2.AutoSize = true;
+            this.btCapNhat2.Location = new System.Drawing.Point(289, 408);
+            this.btCapNhat2.Name = "btCapNhat2";
+            this.btCapNhat2.Size = new System.Drawing.Size(160, 27);
+            this.btCapNhat2.TabIndex = 3;
+            this.btCapNhat2.Text = "Cập nhật sản phẩm c2";
+            this.btCapNhat2.UseVisualStyleBackColor = true;
+            this.btCapNhat2.Click += new System.EventHandler(this.btCapNhat2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 460);
+            this.Controls.Add(this.cbbDanhMuc);
+            this.Controls.Add(this.btCapNhat2);
+            this.Controls.Add(this.btCapNhat);
+            this.Controls.Add(this.lvSanPham);
+            this.Controls.Add(this.tbGia);
+            this.Controls.Add(this.tbTen);
+            this.Controls.Add(this.tbMa);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView lvSanPham;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbMa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbGia;
+        private System.Windows.Forms.Button btCapNhat;
+        private System.Windows.Forms.ComboBox cbbDanhMuc;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btCapNhat2;
+    }
+}
+
---
+++ Bai63/Bai63/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Bai63
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = null;
+        string sqlConn =
+            "SERVER=PAIDEPTRAI\\SQLEXPRESS; Database = CSDLTest; Integrated Security = True";
+        int madm = -1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HienThiDanhMuc();
+        }
+
+        private void btCapNhat_Click(object sender, EventArgs e)
+        {
+            if (conn == null)
+                conn = new SqlConnection(sqlConn);
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            string sql = "update SanPham set Ten = N'" + tbTen.Text 
+                + "', DonGia=" + tbGia.Text + " where Ma = " + tbMa.Text;
+            command.CommandText = sql;
+            command.Connection = conn;
+            int ret = command.ExecuteNonQuery();
+            if (ret > 0)
+            {
+                HienThiSanPhamTheoMaDanhMuc(madm);
+                MessageBox.Show("Cập nhật thành công");
+            }
+            else
+            {
+                MessageBox.Show("Cập nhật thất bại");
+            }
+            
+        }
+
+        private void HienThiDanhMuc()
+        {
+            if (conn == null)
+                conn = new SqlConnection(sqlConn);
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "select * from DanhMuc";
+            command.Connection = conn;
+
+            SqlDataReader reader = command.ExecuteReader();
+            cbbDanhMuc.Items.Clear();
+            while (reader.Read())
+            {
+                int ma = reader.GetInt32(0);
+                string ten = reader.GetString(1);
+                cbbDanhMuc.Items.Add(ma + "-" + ten);
+            }
+            reader.Close();
+        }
+
+        private void cbbDanhMuc_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbDanhMuc.SelectedIndex == -1) return;
+            string line = cbbDanhMuc.SelectedItem + "";
+            string[] arr = line.Split('-');
+            int madm = int.Parse(arr[0]);
+            HienThiSanPhamTheoMaDanhMuc(madm);
+            
+        }
+
+        private void HienThiSanPhamTheoMaDanhMuc(int madm)
+        {
+            if (conn == null)
+                conn = new SqlConnection(sqlConn);
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "select * from SanPham where MaDanhMuc = "+madm;
+            command.Connection = conn;
+            lvSanPham.Items.Clear();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                ListViewItem lvi = new ListViewItem(reader.GetInt32(0) + "");
+                lvi.SubItems.Add(reader.GetString(1));
+                lvi.SubItems.Add(reader.GetInt32(2) + "");
+
+                lvSanPham.Items.Add(lvi);
+            }
+            reader.Close();
+
+        }
+
+        private void lvSanPham_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvSanPham.SelectedItems.Count == 0) return;
+            ListViewItem lvi = lvSanPham.SelectedItems[0];
+            tbMa.Text = lvi.SubItems[0].Text;
+            tbTen.Text = lvi.SubItems[1].Text;
+            tbGia.Text = lvi.SubItems[2].Text;
+        }
+
+        private void btCapNhat2_Click(object sender, EventArgs e)
+        {
+            if (conn == null)
+                conn = new SqlConnection(sqlConn);
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            string sql = "update SanPham set Ten=@ten, DonGia=@gia where Ma=@Ma";
+            command.CommandText = sql;
+            command.Connection = conn;
+
+            command.Parameters.Add("@ten", SqlDbType.NVarChar).Value = tbTen.Text;
+            command.Parameters.Add("@gia", SqlDbType.Int).Value = tbGia.Text;
+            command.Parameters.Add("@ma", SqlDbType.Int).Value = tbMa.Text;
+
+            int ret = command.ExecuteNonQuery();
+            if (ret > 0)
+            {
+                HienThiSanPhamTheoMaDanhMuc(madm);
+                MessageBox.Show("Cập nhật được rồi đó");
+            }
+            else
+            {
+                MessageBox.Show("Cập nhật thất bại");
+            }
+        }
+    }
+}
